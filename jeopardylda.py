eafb00869b584580b7d391482a2ea203b6521e3d
vasumv <vasumvikram@gmail.com>
2015-06-19 23:41:28 -0700

    finished counts

--- jeopardylda.py
+++ jeopardylda.py
@@ -7,20 +7,24 @@ import sqlite3
 counts = {}
 conn = sqlite3.connect("clues.db")
 c = conn.cursor()
-
-c.execute("SELECT clue FROM documents")
-clues = c.fetchall()
+clues_id = {}
+c.execute("SELECT clues.id FROM clues")
+clue_ids = c.fetchall()
+clue_ids = [x[0] for x in clue_ids]
 c.execute("SELECT answer FROM documents")
 answers = c.fetchall()
-
+clues = []
+for id in clue_ids:
+    c.execute(" SELECT clue FROM clues JOIN documents ON clues.id = documents.id WHERE clues.id = %d" % id)
+    clues_id[id] = c.fetchone()[0]
 stop = stopwords.words('english')
-for clue in clues:
-    counts[clue] = {}
-    words = word_tokenize(clue[0])
+for id, clue in clues_id.items():
+    counts[id] = {}
+    words = word_tokenize(clue)
     words = [x.encode('utf-8') for x in words if x.encode('utf-8').isalpha() and x.encode('utf-8') not in stop]
     for word in words:
-        if word not in counts[clue]:
-            counts[clue][word] = 0
-        counts[clue][word] += 1
-
-print counts
+        if word not in counts[id]:
+            counts[id][word] = 0
+        counts[id][word] += 1
+with open("counts.json", "w") as f:
+    f.write(json.dumps(counts, sort_keys=True,indent=4, separators=(',', ': ')))

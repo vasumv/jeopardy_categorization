fbb751e8fa2f1bf91e405a9a68d99b35dd5f6b04
vasumv <vasumvikram@gmail.com>
2015-06-20 00:47:04 -0700

    latest code

--- jeopardylda.py
+++ jeopardylda.py
@@ -15,17 +15,31 @@ clue_ids = [x[0] for x in clue_ids]
 c.execute("SELECT answer FROM documents")
 answers = c.fetchall()
 clues = []
-for id in clue_ids:
-    c.execute(" SELECT clue FROM clues JOIN documents ON clues.id = documents.id WHERE clues.id = %d" % id)
-    clues_id[id] = c.fetchone()[0]
-stop = stopwords.words('english')
-for id, clue in clues_id.items():
-    counts[id] = {}
-    words = word_tokenize(clue)
-    words = [x.encode('utf-8') for x in words if x.encode('utf-8').isalpha() and x.encode('utf-8') not in stop]
-    for word in words:
-        if word not in counts[id]:
-            counts[id][word] = 0
-        counts[id][word] += 1
-with open("counts.json", "w") as f:
-    f.write(json.dumps(counts, sort_keys=True,indent=4, separators=(',', ': ')))
+#for id in clue_ids:
+    #c.execute(" SELECT clue FROM clues JOIN documents ON clues.id = documents.id WHERE clues.id = %d" % id)
+    #clues_id[id] = c.fetchone()[0]
+#stop = stopwords.words('english')
+#for id, clue in clues_id.items()[0:500]:
+    #counts[id] = {}
+    #words = word_tokenize(clue)
+    #words = [x.encode('utf-8') for x in words if x.encode('utf-8').isalpha() and x.encode('utf-8') not in stop]
+    #for word in words:
+        #if word not in counts[id]:
+            #counts[id][word] = 0
+        #counts[id][word] += 1
+with open("lesscounts.json", "r") as fp:
+    counts = json.loads(fp.read())
+#less = {k:v for k, v in counts.items()[0:500]}
+#with open("lesscounts.json", "w") as f:
+    #f.write(json.dumps(less, sort_keys=True,indent=4, separators=(',', ': ')))
+total = 0
+for id in counts:
+    total += len(counts[id].values())
+matrix = np.zeros([len(counts.keys()), total])
+for row in matrix:
+    for id in counts:
+        row[0:len(counts[id].values())] = counts[id].values()
+U, s, V = np.linalg.svd(matrix, full_matrices=True)
+print U.shape, s.shape, V.shape
+#with open("counts.json", "w") as f:
+    #f.write(json.dumps(counts, sort_keys=True,indent=4, separators=(',', ': ')))p

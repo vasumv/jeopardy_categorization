993993b8c5fa08c2eaa67902ad2cc39536f5a397
vasumv <vasumvikram@gmail.com>
2015-07-24 12:12:35 -0700

    latest code for getting lotta results

--- analysis.py
+++ analysis.py
@@ -1,32 +1,54 @@
 import numpy as np
 import lda
-from sklearn.decomposition import TruncatedSVD
+import hungarian
+import matplotlib.pyplot as plt
+from sklearn.decomposition import TruncatedSVD, PCA
+from sklearn.cluster import KMeans
 from vocab import Vocabulary
 from scipy.sparse import lil_matrix, csr_matrix
 from path import Path
 from argparse import ArgumentParser
 from math import log
 import cPickle as pickle
+
+def euclidean_distance(vect1, vect2):
+    return np.linalg.norm(vect1 - vect2)
+
 def cross_entropy(vect1, vect2):
     entropy = 0
     for i in range(len(vect1)):
         entropy += (-1) * vect1[i] * log(vect2[i])
     return entropy
 
-if __name == "__main__":
+if __name__ == "__main__":
 
     argparser = ArgumentParser()
     argparser.add_argument('--qtype', default="qb")
-    argparser.add_argument('--inputname', default="combined_jeo")
-    argparser.add_argument('--inputname2', default="combined_both")
-    argparser.add_argument('--foldername', default="qbsub7")
+    argparser.add_argument('--inputname', default="jeo_combined_naqt")
+    argparser.add_argument('--inputname2', default="naqt_combined_jeo")
+    argparser.add_argument('--inputname3', default="hungarian")
+    argparser.add_argument('--foldername', default="hungarian_results")
     args = argparser.parse_args()
     path = Path(args.foldername)
     if not path.exists():
         path.mkdir()
-    with open("%s/clues.pkl" % args.inputname, "rb") as f:
-        clues = pickle.load(f)
+    #with open("%s/clues.pkl" % args.inputname, "rb") as f:
+        #clues = pickle.load(f)
     topics = np.load("%s/topics.npy" % args.inputname)
     topics2 = np.load("%s/topics.npy" % args.inputname2)
-    vect1 = topics[]
-
+    print topics.shape, topics2.shape
+    matrix = np.zeros((len(topics), len(topics2)))
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            #matrix[i][j] = euclidean_distance(topics[i], topics2[j])
+            matrix[i][j] = cross_entropy(topics[i], topics2[j]) + cross_entropy(topics2[j], topics[i])
+    matches = hungarian.lap(matrix)
+    matches_dict = {}
+    for i in range(len(matches[0])):
+        matches_dict[i] = matches[0][i]
+    print matches_dict
+    #matches = pickle.load(open("%s/matches.pkl" % args.inputname3, "rb"))
+    total  = 0
+    for i in range(len(matches[0])):
+        total += cross_entropy(topics[i], topics2[matches[0][i]]) + cross_entropy(topics2[matches[0][i]], topics[i])
+    print total
--- qlda.py
+++ qlda.py
@@ -2,6 +2,7 @@ import numpy as np
 import lda
 import sqlite3
 import json
+from tqdm import tqdm
 from sklearn.decomposition import TruncatedSVD
 from vocab import Vocabulary
 import MySQLdb as mdb
@@ -32,16 +33,20 @@ def jeoquery(num):
     return clues
 
 argparser = ArgumentParser()
-argparser.add_argument('--qtype', default="jeo")
+argparser.add_argument('--qtype1', default="jeo")
+argparser.add_argument('--qtype2', default="qb")
 argparser.add_argument('--foldername', default="combined_both")
 argparser.add_argument('--inputname', default="jeodata")
 argparser.add_argument('--inputname2', default="qbdata")
-argparser.add_argument('--inputname3', default="combined_jeo")
+argparser.add_argument('--inputname3', default="combined_jeo_small")
+argparser.add_argument('--inputname4', default="ldatest")
+argparser.add_argument('--inputname5', default="../qparser")
 args = argparser.parse_args()
 inputpath = Path(args.inputname)
 inputpath2 = Path(args.inputname2)
 inputpath3 = Path(args.inputname3)
-#vocab = Vocabulary()
+inputpath4 = Path(args.inputname4)
+inputpath5 = Path(args.inputname5)
 
 #if args.qtype == "jeo":
     #clues = jeoquery(20000)
@@ -49,10 +54,16 @@ inputpath3 = Path(args.inputname3)
     #clues = qbquery(20000)
 clues1 = pickle.load(open(inputpath / "clues.pkl", "rb"))
 clues2 = pickle.load(open(inputpath2 / "clues.pkl", "rb"))
+clues4 = pickle.load(open(inputpath5 / "ocred.pkl", "rb"))
 diff = len(clues1) / len(clues2)
-clues2 = clues2 * diff + clues2[:(len(clues1) - len(clues2 * diff))]
+#clues2 = clues2 * diff + clues2[:(len(clues1) - len(clues2 * diff))]
 print len(clues1), len(clues2)
-clues = clues1 + clues2
+clues1 = clues1[:len(clues4)]
+clues2 = clues2[:len(clues4)]
+print len(clues1), len(clues2), len(clues4)
+oldclues = clues2 + clues4
+clues = clues2 + clues4
+#vocab = Vocabulary()
 #clues = []
 #for i in oldclues:
     #if i not in clues:
@@ -68,12 +79,30 @@ print vocab.number
 matrix = lil_matrix((len(clues), vocab.number + 1))
 matrix = matrix.astype(np.int64)
 i = 0
-for clue in clues:
+for clue in tqdm(clues):
     vector = vocab.translate(clue)
     matrix[i] = vector
     i += 1
 print matrix.shape
 matrix = csr_matrix(matrix)
+#np.save(path / "matrix.npy", matrix)
+#matrix = pickle.load(open(inputpath4 / "matrix.pkl", "rb"))
+#print matrix.shape
+#idx = np.arange(matrix.shape[1])
+#print idx.shape
+
+#with open("docformat.txt", "w") as f:
+    #for i in xrange(matrix.shape[0]):
+        #row = matrix[i].todense()
+        #print row.shape
+        #word_idx = row > 0
+        #words = idx[word_idx]
+        #unique = words.shape[0]
+        #counts = zip(words, row[word_idx])
+        #print >> f, "%d %s" % (unique,
+                               #' '.join(
+                                   #["%d:%d" % (t, c) for t, c in counts]
+                               #))
 #svd = TruncatedSVD(n_components=700)
 #docs = svd.fit_transform(matrix)
 model = lda.LDA(n_topics=10, n_iter=10000)
@@ -82,7 +111,7 @@ docs = model.fit_transform(matrix)
 top_words = 8
 
 with open(path / "topicslist.txt", "w") as f:
-    f.write("Combined List of 10 Topics \n\n")
+    f.write("Combined List of 7 Topics \n\n")
 for i, topic_dist in enumerate(model.topic_word_):
     words = [vocab.number_mapping[w] for w in topic_dist.argsort()[::-1][:top_words]]
     with open(path / "topicslist.txt", "a") as f:
